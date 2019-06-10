5ba9b3bd806a1c830d62f48121872caea0c8dfbb
Lashawnda Bynum <51242295+lbynum1@users.noreply.github.com>
2019-06-10 02:14:08 -0400

    Update ufoheatmap.py

--- ufoheatmap.py
+++ ufoheatmap.py
@@ -10,14 +10,10 @@ places = []
 dic = {}
 final = []
 
-# open the file for dictionary
-data = open("bynum_lashawnda_cities.txt", "wb")
+out = open("bynum_lashawnda_cities.txt", "wb")
 
-# page with all url links
-main = requests.get("http://www.nuforc.org/webreports/ndxevent.html")
-
-# get all the links on the main page by parsing through it
-soup = BeautifulSoup(main.text, "html.parser")
+url = requests.get("http://www.nuforc.org/webreports/ndxevent.html")
+soup = BeautifulSoup(url.text, "html.parser")
 row = soup.findAll("a")
 
 
@@ -26,38 +22,38 @@ for i in range(1, len(row)):
 
     link_ = "http://www.nuforc.org/webreports/" + link
     url_2 = requests.get(link_)
-    soup_2 = BeautifulSoup(url_2.text, "html.parser")
 
+
+    soup_2 = BeautifulSoup(url_2.text, "html.parser")
     # time.sleep(5)
 
     for record in soup_2.findAll('tr'):
         location.append([record.text])
 
+
 for i in location[1:]:
     for element in i:
         data = element.split("\n")
-
-# this gets the cities and states
         cities = data[2] + "," + data[3]
         places.append(cities)
 
-# this counts the number of reports in each city and state
 for i in range(0, len(places)):
     if places[i] in dic:
         dic[places[i]] += 1
+
     else:
         dic[places[i]] = 1
 
-# this enables a tab between the reports and each city and state
+
 for a,b in dic.items():
     string = a + " " + str(b)
     final.append(string)
-each = '\n'.join(final)
+fin = '\n'.join(final)
 
 
-pin = open("bynum_lashawnda_cities.txt", "rb")
-pickle.dump(each, out)
-fin = pickle.load(pin)
-print(fin)
+key = open("bynum_lashawnda_cities.txt", "rb")
+pickle.dump(tb, out)
+lock = pickle.load(key)
+print(lock)
 
 out.close()
